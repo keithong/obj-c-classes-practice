8ec7c314db3bc5733bdfaf698307fc0b95113b09
Keith Samson <k.samson@klab.com>
2014-06-04 14:19:56 +0800

    Changed information between Asset and Employee class

--- Employee.h
+++ Employee.h
@@ -8,13 +8,16 @@
 
 #import <Foundation/Foundation.h>
 #import "Person.h"
+@class Asset;
+
 @interface Employee : Person
 {
     int employeeID;
-    NSString *lastName;
-    Person *spouse;
-    NSMutableArray *children;
+    NSMutableArray *assets;
     
 }
 @property int employeeID;
+-(void)addAssetsObject:(Asset *)a;
+-(unsigned int)valueOfAssets;
+
 @end
