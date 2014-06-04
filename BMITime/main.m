6e484825f3f227afa941b7c20466105a1029e82b
Keith Samson <k.samson@klab.com>
2014-06-04 12:07:38 +0800

    Added comments in main.m and modified the code a little

--- BMITime/main.m
+++ BMITime/main.m
@@ -14,12 +14,16 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
        
+        //Create an instance of Person
         Person *p = [[Person alloc]init];
         
+        //Give the instance variables interesting values
         [p setWeightInKilos:45];
         [p setHeightInMeters:1.72];
         
-        NSLog(@"This person's BMI is %f", [p bodyMassIndex]);
+        //Call the bodyMassIndex method
+        float bmi = [p bodyMassIndex];
+        NSLog(@"This person's BMI is %.2f", bmi);
         
     }
     return 0;
