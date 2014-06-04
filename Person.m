9ae5346641f1905ceae27771a18db57b9a276efd
Keith Samson <k.samson@klab.com>
2014-06-04 11:56:56 +0800

    Added the implementation of Person.h

--- Person.m
+++ Person.m
@@ -9,5 +9,16 @@
 #import "Person.h"
 
 @implementation Person
+-(void)setHeightInMeters:(float)h{
+    heightInMeters = h;
+}
+
+-(void)setWeightInKilos:(int)w{
+    weightInKilos = w;
+}
+
+-(float)bodyMassIndex{
+    return weightInKilos / (heightInMeters * heightInMeters);
+}
 
 @end
